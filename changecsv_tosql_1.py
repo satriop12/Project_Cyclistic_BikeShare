382f0d1bcd4d1f550aebf49cb51430b113491d64
Muhammad Satrio Pamungkas Suharoyo <142678659+satriop12@users.noreply.github.com>
2023-10-03 17:36:47 +0700

    Add files via upload

---
+++ changecsv_tosql_1.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd
+
+# initialize the sqlite connection
+connect = sqlite3.connect('cyclistic_bikeshare_q1_2023_3month.db')
+# loop through each csv file and append data to the sqlite table
+csv_files = ['202301-divvy-tripdata.csv', '202302-divvy-tripdata.csv', '202303-divvy-tripdata.csv']
+
+for file in csv_files:
+    # read data from the csv file and parse datetime columns
+    df = pd.read_csv(file, parse_dates=["started_at", "ended_at"], encoding="utf-8")
+    df.columns = df.columns.str.strip()
+    # append the data to the sqlite table
+    df.to_sql('data_bikeshare_q1_2023', connect, if_exists='append', index=False)
+
+print("Finish!")
