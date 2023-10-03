c7cba69f4c960c7a1937e30f17ebcc37395c3402
Muhammad Satrio Pamungkas Suharoyo <142678659+satriop12@users.noreply.github.com>
2023-10-03 17:37:29 +0700

    Add files via upload

---
+++ fromdb_tocsv.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas as pd
+
+# connect to the sqlite database
+conn = sqlite3.connect('cyclistic_bikeshare_q1_2023_3month.db')
+
+# define the sql query to retrieve data from your table
+query = "SELECT * FROM data_bikeshare_q1_2023"
+
+# use pandas to read the data from the sqlite table into a dataframe
+df = pd.read_sql_query(query, conn)
+
+# define the filename for your csv file
+csv_filename = '2023-q1-divvy-tripdata.csv'
+
+# export the DataFrame to a cssv file
+df.to_csv(csv_filename, index=False)
+
+# close the database connection
+conn.close()
+
+print(f'Data exported to {csv_filename}')
